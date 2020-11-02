71195acb86d1d97cdb9ac6c6deb1096fdf079a8c
Gulnavaz wali <Gulnavazwali1@hotmail.com>
2020-11-02 13:55:17 +0100

    Added controllers

---
+++ BlogProject/Controllers/BlogController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BlogProject.DAL;
+using BlogProject.Models;
+
+namespace BlogProject.Controllers
+{
+    [Route("api/blog")]
+    public class BlogController : ApiController
+    {
+        private BlogContext db = new BlogContext();
+
+        // GET: api/Blog
+        public IQueryable<BlogPost> GetBlogPosts()
+        {
+            return db.BlogPosts;
+        }
+
+        // GET: api/Blog/5
+        [ResponseType(typeof(BlogPost))]
+        public async Task<IHttpActionResult> GetBlogPost(int id)
+        {
+            BlogPost blogPost = await db.BlogPosts.FindAsync(id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(blogPost);
+        }
+
+        // PUT: api/Blog/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutBlogPost(int id, BlogPost blogPost)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != blogPost.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(blogPost).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BlogPostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Blog
+        [ResponseType(typeof(BlogPost))]
+        public async Task<IHttpActionResult> PostBlogPost(BlogPost blogPost)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.BlogPosts.Add(blogPost);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = blogPost.ID }, blogPost);
+        }
+
+        // DELETE: api/Blog/5
+        [ResponseType(typeof(BlogPost))]
+        public async Task<IHttpActionResult> DeleteBlogPost(int id)
+        {
+            BlogPost blogPost = await db.BlogPosts.FindAsync(id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            db.BlogPosts.Remove(blogPost);
+            await db.SaveChangesAsync();
+
+            return Ok(blogPost);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BlogPostExists(int id)
+        {
+            return db.BlogPosts.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ BlogProject/Service/BlogService.cs
@@ -0,0 +1,32 @@
+﻿using BlogProject.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BlogProject.Service
+{
+    public class BlogService
+    {
+        static HttpClient httpClient = new HttpClient();
+        private static string GetPostBlogs = "https://localhost:44357";
+        private static string EditPostBlogs = "https://localhost:44357";
+        private static string DeletePostBlogs = "https://localhost:44357";
+        public async Task<ObservableCollection<BlogPost>> GetPostBlogs()
+        {
+            
+        }
+        public async Task<ObservableCollection<BlogPost>> EditPostBlogs()
+        {
+
+        }
+        public async Task<ObservableCollection<BlogPost>> DeletePostBlogs()
+        {
+
+        }
+    }
+}
